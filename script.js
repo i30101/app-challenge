eb414d69f35d0c162a19f2d39f95c876c41d2515
i30101 <andrewkim229@gmail.com>
2022-10-29 21:07:16 -0400

    updated script and timer

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-var check = null;
-var cnt = 1500;
+let time = document.getElementById("time");
+let play = document.getElementById("play");
+let check = null;
+let cnt = 1500;
+let second, minute;
 
+function updateTime() {
+    second = cnt % 60;
+    minute = Math.floor(cnt / 60) % 60;
+    minute = (minute > 59) ? minute = 59 : minute = minute;
+    second = (second < 10) ? '0'+second : second;
+    minute = (minute < 10) ? '0'+minute : minute;
+    time.innerHTML = `${minute}:${second}`;
+}
 
 function printDuration() {
-    if (check == null) {
 
+    if (check == null) {
+        play.innerHTML = `<span class="material-symbols-outlined toggle-icon">pause</span>`;
 
         check = setInterval(function () {
             cnt -= 1;
-            var second = cnt % 60;
-            var minute = Math.floor(cnt / 60) % 60;
-            minute = (minute > 59) ? minute = 59 : minute = minute;
-            second = (second < 10) ? '0'+second : second;
-            minute = (minute < 10) ? '0'+minute : minute;
-    
-            document.getElementById("time").innerHTML = `${minute}:${second}`;
+            // second = cnt % 60;
+            // minute = Math.floor(cnt / 60) % 60;
+            // minute = (minute > 59) ? minute = 59 : minute = minute;
+            // second = (second < 10) ? '0'+second : second;
+            // minute = (minute < 10) ? '0'+minute : minute;
+            // time.innerHTML = `${minute}:${second}`;
+            updateTime();
         }, 1000);
+    }else {
+        stop();
+        play.innerHTML = `<span class="material-symbols-outlined toggle-icon">play_arrow</span>`;
     }
 }
 
 function stop() {
     clearInterval(check);
     check = null;
-    // document.getElementById("time").innerHTML = '0';
+    // time.innerHTML = '0';
 }
 
-
+function reset() {
+    clearInterval(check);
+    cnt = 1500;
+    updateTime();
+}
